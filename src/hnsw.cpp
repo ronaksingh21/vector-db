895f67f86e4f55780a9149a3de23718762df7271
ronak singh <singhronak2008@gmail.com>
2026-08-30 19:51:47 -0700

    implement insert method for HNSW class to add new nodes and connect neighbors

--- src/hnsw.cpp
+++ src/hnsw.cpp
@@ -1,3 +1,9 @@
+#include "hnsw.h"
+#include <vector>
+#include <cmath>
+#include<random>
+#include <algorithm>
+
 HNSW::HNSW(int max_layers, int M, int ef_construction)
     : max_layers(max_layers), M(M), ef_construction(ef_construction){
     ml = 1.0/ std::log(2.0);
@@ -16,6 +22,37 @@ int HNSW::get_random_layer(){
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0.0,1.0);
-    int layer = int()(-std::log(dis(gen))*ml);
+    int layer = (int)(-std::log(dis(gen))*ml);
     return std::min(layer, max_layers-1);
+}
+
+void HNSW::insert(int id, const std::vector<float>& vec) {
+    Node new_node = {id, vec, {}, get_random_layer()};
+    if(entry_point == -1){
+        entry_point = id;
+        data[id]= new_node;
+        return;
+    }
+    Node& entry = data[entry_point];
+    for( int layer = entry.current_layer; layer>= new_node.current_layer; layer--){
+        std::vector <std::pair<float,int>> distances;
+        for (auto& [node_id,node] : data){
+            if(node.current_layer>= layer){
+                float dist = l2_distance(new_node.vec, node.vec);
+                distances.push_back({dist,node_id});
+            }
+        }
+        std::sort(distances.begin(), distances.end());
+    
+        // connect to m closest neighbors bidirectionally
+        for (int i = 0; i < std::min(M, (int)distances.size()); i++) {
+            int neighbor_id = distances[i].second;
+            new_node.neighbors_by_layer[layer].push_back(neighbor_id);
+            data[neighbor_id].neighbors_by_layer[layer].push_back(id);
+        }
+    }
+    if (new_node.current_layer > entry.current_layer){
+        entry_point = id;
+    }
+    data[id] = new_node;
 }
